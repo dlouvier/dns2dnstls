dff125ae5cc9c3f1b77314395776956a618bdaad
dani <dani@patagonia>
2019-02-15 14:26:31 +0100

    Isolating just the TCP Server to work on

--- server.go
+++ server.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"encoding/binary"
 	"log"
 	"net"
 	"strconv"
-
-	"golang.org/x/net/dns/dnsmessage"
 )
 
 // TCPServer - Start a server listening under TCP Protocol
@@ -29,36 +26,36 @@ func TCPServer() {
 }
 
 // UDPServer - Start a server listening on UDP Protocol
-func UDPServer() {
-	srv, err := net.ListenUDP("udp", &net.UDPAddr{IP: net.ParseIP((HostListenAddress)), Port: HostPort})
-	Debugger(err)
+// func UDPServer() {
+// 	srv, err := net.ListenUDP("udp", &net.UDPAddr{IP: net.ParseIP((HostListenAddress)), Port: HostPort})
+// 	Debugger(err)
 
-	// Close the listener when the application closes.
-	defer srv.Close()
+// 	// Close the listener when the application closes.
+// 	defer srv.Close()
 
-	log.Println("DNS Proxy server started on " + HostListenAddress + ":" + strconv.Itoa(HostPort) + " (UDP)")
+// 	log.Println("DNS Proxy server started on " + HostListenAddress + ":" + strconv.Itoa(HostPort) + " (UDP)")
 
-	for {
-		buffer := make([]byte, 512)
-		PkgSizeInt, addr, err := srv.ReadFromUDP(buffer)
-		Debugger(err)
-		log.Println("UDP Request received")
+// 	for {
+// 		buffer := make([]byte, 512)
+// 		PkgSizeInt, addr, err := srv.ReadFromUDP(buffer)
+// 		Debugger(err)
+// 		log.Println("UDP Request received")
 
-		// I need to create an TCP package, which requires to know the size of the package
-		PkgSize := make([]byte, 2)
-		binary.BigEndian.PutUint16(PkgSize, uint16(PkgSizeInt))
+// 		// I need to create an TCP package, which requires to know the size of the package
+// 		PkgSize := make([]byte, 2)
+// 		binary.BigEndian.PutUint16(PkgSize, uint16(PkgSizeInt))
 
-		buffer = append(PkgSize, buffer[:510]...)
+// 		buffer = append(PkgSize, buffer[:510]...)
 
-		//Query the external DNS resolver
-		ans := QueryForwarder(buffer)
+// 		//Query the external DNS resolver
+// 		ans := QueryForwarder(buffer)
 
-		var m dnsmessage.Message
-		err = m.Unpack(ans[2:])
-		Debugger(err)
+// 		var m dnsmessage.Message
+// 		err = m.Unpack(ans[2:])
+// 		Debugger(err)
 
-		ans, _ = m.Pack()
+// 		ans, _ = m.Pack()
 
-		_, err = srv.WriteToUDP(ans, addr)
-	}
-}
+// 		_, err = srv.WriteToUDP(ans, addr)
+// 	}
+// }
