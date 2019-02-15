4e7311236b99b3653673ae8c085a901e73a49da0
dani <dani@patagonia>
2019-02-15 14:25:10 +0100

    Allow to use easyly locally

--- main.go
+++ main.go
@@ -4,9 +4,9 @@ const (
 	// HostListenAddress - Listen address to export the service
 	HostListenAddress string = "0.0.0.0"
 	// HostPort - Port where the service is exposing
-	HostPort int = 53
+	HostPort int = 1111
 	// ForwarderDNS - External DNS resolver
-	ForwarderDNS = "1.0.0.1:853" // 1.0.0.1 as my ISP (o2) blocks 1.1.1.1 and 1.0.0.0 addresses
+	ForwarderDNS string = "1.0.0.1:853" // 1.0.0.1 as my ISP (o2) blocks 1.1.1.1 and 1.0.0.0 addresses
 )
 
 func main() {
